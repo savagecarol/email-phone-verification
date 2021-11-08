0b354cd9bfe92d3f2290efafe3f4a40a97494087
kartikeya Sharma <karthiksharma1411@gmail.com>
2021-11-08 10:27:51 +0530

    only otp web working

--- otp-web/src/main/java/com/example/otpweb/Dao/EmailDao.java
+++
@@ -1,6 +0,0 @@
-package com.example.otpweb.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.otpweb.entity.Email;
-public interface EmailDao extends JpaRepository<Email, String>
-{}
--- otp-web/src/main/java/com/example/otpweb/Dao/NumberDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.otpweb.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.otpweb.entity.PhoneNumber;
-
-public interface NumberDao extends JpaRepository<PhoneNumber, String>
-{}
