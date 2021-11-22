b0d39ee2bb46ae37cb6e2de540ecebb89657d0fd
savagecarol <karthiksharma1411@gmail.com>
2021-11-22 13:07:40 +0530

    Dsf

--- src/main/java/com/example/otpweb/config/SwaggerConfig.java
+++ src/main/java/com/example/otpweb/config/SwaggerConfig.java
@@ -17,6 +17,7 @@ import springfox.documentation.service.Contact;
 @EnableSwagger2
 public class SwaggerConfig {
 
+
 	@Bean
 	public Docket swaggerApiConfig() {
 		return new Docket(DocumentationType.SWAGGER_2).select().paths(PathSelectors.any())
